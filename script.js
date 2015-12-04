c93e5f07fd130b099bf16afd9bff46b021a5eb37
Nicholas Castaneda <npcastaneda@gmail.com>
2015-12-04 10:41:32 -0800

    Added modal

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // track of game progress, warranted a var for whichever suspect the
 // player has a warrant for, and rank is the current rank of the player.
 var hoursLeft = 110;
-var name = prompt("What's your name?");
+var name = '';
 var round = 1;
 var warranted = '';
 var rank = 'Gumshoe';
@@ -81,6 +81,45 @@ function newGame(){
   guiltySuspect = allSuspects[Math.floor(Math.random()*20)];
   office.changeAll();
 }
+
+//////////////////////Modal Code//////////////
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+$(document).ready(function($) {
+  openFunc();
+});;
+function openFunc() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {closeFunc()};
+function closeFunc() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event){
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+$('#nameSubmit').click(function(){
+  closeFunc();
+  name = document.getElementById("nameInput").value;
+  document.getElementById('player').innerHTML = name;
+})
+
+
+
 
 ////////////////////////////////////////////////////////////////////////
 //////////              Criminals and Suspects                //////////
